3252c276da6515bf2fae227f554bb69a51bccd02
XxXtomisek07XxX <42677459+XxXtomisek07XxX@users.noreply.github.com>
2018-11-29 16:00:59 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,6 +21,24 @@ async def on_ready():
 	print(bot.user.name)
 	print(bot.user.id)
 	
+@bot.command(pass_context=True)  
+@commands.has_permissions(kick_members=True)     
+
+async def kick(ctx,user:discord.Member):
+
+    if user.server_permissions.kick_members:
+        await bot.say('**Nemáš Oprávnění na kick a nebo je dotyčný Administrátor**')
+        return
+    
+    try:
+        await bot.kick(user)
+        await bot.say(user.name+' byl kicknut. Měj se '+user.name+'!')
+        await bot.delete_message(ctx.message)
+
+    except discord.Forbidden:
+        await bot.say('Permission denied.')
+        return	
+	
 
 bot.run(os.getenv("BOT_TOKEN"))
 
