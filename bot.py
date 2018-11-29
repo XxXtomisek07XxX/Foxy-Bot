5baa24ecda4bbbc3f555a8ce26b578cf609fc58f
XxXtomisek07XxX <42677459+XxXtomisek07XxX@users.noreply.github.com>
2018-11-29 16:45:12 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,43 +1,76 @@
-import discord 
+import discord
 from discord.ext.commands import Bot
-from discord.ext import commands 
-from discord.ext.commands.cooldowns import BucketType 
-import asyncio 
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+
+import asyncio
 import platform
 import colorsys
 import random
-import os
+import os 
 import time
- 
-import discord
-from discord.ext import commands
-import asyncio
 
-bot=commands.Bot(command_prefix='f!')
+
+
+
+
+bot=commands.Bot(command_prefix='/')
+
+
 
 @bot.event
 async def on_ready():
-	print('The bot is ready!')
-	print(bot.user.name)
-	print(bot.user.id)
-	
-@bot.command(pass_context=True)  
-@commands.has_permissions(kick_members=True)     
+     await bot.change_presence(game=discord.Game(name= "/help"))
+     
+   
+        
 
-async def kick(ctx,user:discord.Member):
-
-    if user.server_permissions.kick_members:
-        await bot.say('**Nemáš Oprávnění na kick a nebo je dotyčný Administrátor**')
-        return
+@bot.event
+async def on_ready():
     
-    try:
-        await bot.kick(user)
-        await bot.say(user.name+' byl kicknut. Měj se '+user.name+'!')
-        await bot.delete_message(ctx.message)
 
-    except discord.Forbidden:
-        await bot.say('Permission denied.')
-        return	
+  print('The bot is ready!')
+  print(bot.user.name)
+  print(bot.user.id)
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+
+    
+
+
+
+   
+     
+        
+    
+    
+       
+        
+        
+
+    
+       
+       
 	
 
 bot.run(os.getenv("BOT_TOKEN"))
